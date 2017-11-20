25f0014007bbf57be722f88b59d5441e5df92187
Oleg Kuzmich <31180225+kuzmich-oleg@users.noreply.github.com>
2017-11-20 10:25:07 +0200

    Update lab1.3.1

--- CPA Programing Essentials in C++/Chapter1/Lab 1.3/lab 1.3.1.cpp
+++ CPA Programing Essentials in C++/Chapter1/Lab 1.3/lab 1.3.1.cpp
@@ -4,9 +4,9 @@ using namespace std;
 //start point of the program
 int main() {
 	
-	int seconds = 60;
-	int minutes = 3;
-	
-	cout << minutes << " minutes contain " << seconds << " seconds";
+	int v = 10800; // 3*60*60
+	int zzz= 3 * 60;  // This is a variable to hold the value of 3 minutes in seconds
+	int z = 5 * 60;
+	float pi = 3.141526; //This is the value of pi
 	return 0;
 }
